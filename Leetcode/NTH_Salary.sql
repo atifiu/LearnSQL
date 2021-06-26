c30262a15c68c9d0ad44dd21e6a2a28748bbd7d7
atifiu <48252551+atifiu@users.noreply.github.com>
2021-06-26 17:36:38 +0530

    Update NTH_Salary.sql
    Added the function code with comments

--- Leetcode/NTH_Salary.sql
+++ Leetcode/NTH_Salary.sql
@@ -16,3 +16,67 @@ For example, given the above Employee table, the nth highest salary where n = 2 
 | 200                    |
 +------------------------+
 */
+
+CREATE FUNCTION getNthHighestSalary (N IN NUMBER) RETURN NUMBER 
+IS
+result NUMBER;
+BEGIN
+
+/*Option 1 using fetch option available only in 12c onwards but will not give correct result in all cases
+
+WITH cte1 AS 
+    (
+    SELECT salary 
+    FROM employee 
+    ORDER BY salary DESC 
+    FETCH FIRST N ROWS ONLY
+    )
+SELECT salary
+INTO result
+FROM cte1 
+ORDER BY salary 
+Fetch First 1 Rows Only*/
+
+
+/*Option 2 using tradiontal style 
+	
+	SELECT MAX(salary)
+    INTO result
+    FROM employee a 
+    WHERE N-1 = (SELECT COUNT(DISTINCT salary)
+                    FROM employee b WHERE b.salary > a.salary);*/
+
+
+/*Option 3 using dense_rank with distinct                                                        
+  WITH cte1 AS(
+    SELECT salary, 
+    DENSE_RANK() OVER(ORDER BY Salary DESC) AS rn 
+    FROM EMPLOYEE
+    )
+    SELECT DISTINCT salary 
+    INTO result 
+    FROM cte1 
+    WHERE rn = N;*/
+    
+    
+    /*Option 4 using dense_rank with aggregate function max 
+    
+     WITH cte1 AS(
+    SELECT salary, 
+    DENSE_RANK() OVER(ORDER BY Salary DESC) AS rn 
+    FROM EMPLOYEE
+    )
+     SELECT  MAX(salary)    
+    INTO Result 
+    FROM cte1
+    WHERE rn = N;*/
+    
+														   
+   
+RETURN result;
+
+EXCEPTION WHEN
+NO_DATA_FOUND THEN
+RETURN NULL;
+       
+END;
